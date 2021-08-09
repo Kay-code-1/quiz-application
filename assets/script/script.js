a1dfaa141c158a2848b36baac4cd4f184f279ac7
kalyani joshi <kalyani121@gmail.com>
2021-08-09 09:38:29 -0400

    changes to question logic to fill inner HTML

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,82 +1,80 @@
 let score = 0;
 let timer = 0;
-var question = document.querySelector(".question");
-var availableOptions = document.querySelectorAll(".option");
+var quizQuestion = document.querySelector(".question");
+var quizOptions = document.querySelectorAll(".option");
 
-let questionArray = [
-  {
+//JSON array for questions
+
+let questionArray = '{"questions":['+
+'{
     question: "Joey played Dr. Drake Ramoray on which soap opera show?",
-    option1: "Stranger Things",
-    option2: "Days of Our Lives",
-    option3: "Breaking Bad",
-    option4: "Prison Break",
+    optionArray: [
+      "Stranger Things",
+      "Days of Our Lives",
+      "Breaking Bad",
+      "Prison Break",
+    ],
     answer: "Days of Our Lives",
-  },
+  }',
 
-  {
+  '{
     question: "Phoebe’s scientist boyfriend David worked in what city?",
-    option1: "Mumbai",
-    option2: "Pune",
-    option3: "Sunnyvale",
-    option4: "Minsk",
+    option: ["Mumbai", "Pune", "Sunnyvale", "Minsk"],
     answer: "Minsk",
-  },
+  }',
 
   {
     question: "What color is Monica’s apartment?",
-    option1: "Blue",
-    option2: "Red",
-    option3: "Pink",
-    option4: "Purple",
+    option: ["Blue", "Red", "Pink", "Purple"],
     answer: "Purple",
   },
 
   {
     question: "Joey and Chandler’s TV guide is addressed to who?",
-    option1: "Miss Chanandler Bong",
-    option2: "Mr Candle Bing",
-    option3: "Mr Chandler Bong ",
-    option4: "Joey Tribbiani",
+    option: [
+      "Miss Chanandler Bong",
+      "Mr Candle Bing",
+      "Mr Chandler Bong ",
+      "Joey Tribbiani",
+    ],
     answer: "Miss Chanandler Bong",
   },
 
   {
     question: " Which character famously said, “PIVOT?”?",
-    option1: "Rachel",
-    option2: "Joey",
-    option3: "Ross",
-    option4: "Gunther",
+    option: ["Rachel", "Joey", "Ross", "Gunther"],
     answer: "Ross",
   },
 
   {
     question:
       "Brad Pitt and David Schwimmer’s characters cofounded what club in high school?",
-    option1: "Acting Club",
-    option2: "Paleontology Club",
-    option3: "Kids Club",
-    option4: "The “I Hate Rachel Green Club.”",
+    option: [
+      "Acting Club",
+      "Paleontology Club",
+      "Kids Club",
+      "The “I Hate Rachel Green Club.”",
+    ],
     answer: "The “I Hate Rachel Green Club.”",
   },
-];
+]
+    
+}
+  
 
-console.log("Total Questions:" + questionArray.length);
-ongoingQuestion = 0;
-
+const questionText = json.parse(questionArray);
 function startQuiz() {
   console.log("Quiz has started!");
 
   for (let i = 0; i < questionArray.length; i++) {
-    question.innerHTML = questionArray[ongoingQuestion]["question"];
-    for (
-      let i = 0;
-      i < questionArray[ongoingQuestion]["availableOptions"].length;
-      i++
-    ) {
-      var number = availableOptions.dataset["number"];
-      availableOptions.innerHTML = ongoingQuestion["availableOptions" + number];
-    }
-    ongoingQuestion++;
+    console.log("Total Questions:" + questionArray.length);
+    
+    quizQuestion.innerHTML = questionText.question[i];
+    // for (let i = 0; i < questionArray.option.length; i++) {
+    //   console.log("options length: " + (questionArray.option).length);
+    
+    // }
+    // ongoingQuestion++;
   }
 }
 
